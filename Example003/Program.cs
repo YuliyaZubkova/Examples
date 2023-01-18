56f2aea8b409ab39a8ff98932ec66a4fe1ef560b
Yuliya <i@yzubkova.ru>
2023-01-19 00:15:40 +0300

    Add random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,3 +1,8 @@
-﻿int numberA = new Rundom().Next(1, 10); //1, 2, 3, 4...9
-int numberB = new Rundom().Next(1, 10);
+﻿int numberA = new Random().Next(1, 10); //1, 2, 3, 4...9
+Console.Write("First number - ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("Second number - ");
+Console.WriteLine(numberB);
+Console.Write("SUM = ");
 Console.WriteLine(numberA + numberB);
