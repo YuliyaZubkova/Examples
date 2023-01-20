19ea6b2269aa92a70a941d9818719890ccd2c77d
Yuliya <i@yzubkova.ru>
2023-01-20 09:27:16 +0300

    Задача про нахождение индекса элемента с определенным значением в массиве

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти индекс элемента с определенным значением
+
+int[] array = {12, 224, 36, 45, 45, 36, 71, 82, 19};
+
+int n = array.Length;
+int find = 36;
+
+int index = 0; // нумерация элементов в массиве начинается с 0 элемента
+
+while (index < n)
+{
+
+if(array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+}
+
+index++; //index = index + 1
+}
