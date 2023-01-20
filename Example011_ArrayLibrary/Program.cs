a8ebbb35aeef646c966e67a3667232a38bf3fc75
Yuliya <i@yzubkova.ru>
2023-01-20 10:00:29 +0300

    Задача про нахождение индекса элемента с определенным значением в массив с использованием метода voidе

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,55 @@
+﻿// 
+
+void FillArray(int[] collection) 
+//collection - аргумент
+//void - если метод (функция) ничего не возвращает 
+//и в коде мы не используем оператор return
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++; //index = index + 1
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    //если элемента нет в списке - значит будет выдавать значение -1 
+    //чтобы не путать 0 с нулевым индексом
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+//new int [10] - означает создать новый массив, 
+//в котором будет 10 элементов
+int[] array = new int [10]; 
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(); // вывод пустой строки
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
