f84cc193fe1aa75d1cda2da4ae6ce6059693c08c
Yuliya <i@yzubkova.ru>
2023-01-20 08:59:27 +0300

    Задача про нахождение максимального числа из девяти дополнена

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,4 +1,5 @@
-﻿// 
+﻿/* Программа нахождения максимального числа из девяти чисел стихийным способом:
+
 int a1 = 31;
 int b1 = 44;
 int c1 = 23;
@@ -21,4 +22,33 @@ if(a3 > max) max = a3;
 if(b3 > max) max = b3;
 if(c3 > max) max = c3;
 
+Console.WriteLine(max);
+*/
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+} 
+
+int a1 = 31;
+int b1 = 58;
+int c1 = 23;
+int a2 = 32;
+int b2 = 43;
+int c2 = 24;
+int a3 = 6752;
+int b3 = 94;
+int c3 = 85;
+
+/* (более длинный вариант)int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+*/
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+ 
 Console.WriteLine(max);
